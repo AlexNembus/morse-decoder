ffd22f2e73ba1da63188cd39c7b6b9e0f1545c52
Alex <alexander98mangus@gmail.com>
2019-09-23 23:11:10 +0300

    feat: morse decoder solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,14 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let morseKey = '';
+    let reslt = '';
+    for ( let i = 0 ; i < expr.length ; i = i + 10 ){
+     morseKey = expr.slice(i, i + 10).replace(/(11)/g , '-').replace(/(10)/g , '.').replace(/(00)/g , '');
+     ( morseKey === '**********' ? reslt = reslt + ' ' : reslt = reslt + MORSE_TABLE[morseKey] )  
+    }
+    console.log(reslt);
+     return reslt;
 }
 
 module.exports = {
